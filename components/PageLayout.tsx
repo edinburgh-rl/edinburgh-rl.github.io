0d3d395bf6ac732fdddd40b3765515d8ca2ddad1
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2025-10-03 16:15:35 +0100

    fix: updated column resizing

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -7,7 +7,7 @@ export default function PageLayout({ children }: { children: ReactNode }) {
         <div className="content primary-content is-flex is-flex-direction-column is-full-height m-0">
             <Navbar />
             <div className="columns is-centered is-flex-grow-1 m-0 p-0" >
-                <div className="column is-four-fifths is-flex is-flex-direction-column p-0">
+                <div className="column is-two-thirds is-flex is-flex-direction-column p-0">
                     <div className="content is-flex-grow-1 p-3">
                         {children}
                     </div>
