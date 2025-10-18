cf82e52197599cecb8957850c9e72592e8f11a03
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2025-10-18 23:35:46 +0100

    fix: added exception to the example article in the sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,9 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://edinburgh-rl.github.io/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  exclude: [
+    '/blog/example'
+  ],
   robotsTxtOptions: {
         policies: [
             {
